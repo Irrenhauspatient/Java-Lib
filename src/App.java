bec0f015db47bac1a05874dabf73b160efea08f8
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-04-23 22:06:27 +0200

    constructor test

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Person person = new Person("asdasd", "asdasdas", 99);
+        Lib_Constructor.createObject("Person");
 
     }
 }
