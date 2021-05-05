c1990b8f6da506f1ea714cd6c433c29afe5a54b8
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-05-05 09:17:34 +0200

    addon

--- src/Lib_File.java
+++ src/Lib_File.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -41,11 +43,29 @@ public class Lib_File {
         return file;
     }
 
-    public static ArrayList<String> readLinebyLine(File file) throws IOException {
+    public static ArrayList<String> readLinebyLine(String filename) throws IOException {
 
+        File file = new File(filename);
         ArrayList<String> list = (ArrayList<String>) Files.readAllLines(Paths.get(file.getAbsolutePath()));
 
         return list;
+
     }
 
+    public static boolean isExistentAndReadibleBoolean(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        if (file.exists() && file.canRead() && !file.isDirectory()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static <T> void writeTextToFile(ArrayList<T> lise, String filename, boolean append) throws IOException {
+
+        FileWriter filewriter = new FileWriter(filename, append);
+        for (T t : lise) {
+            filewriter.write(t.toString() + "\n");
+        }
+        filewriter.close();
+    }
 }
